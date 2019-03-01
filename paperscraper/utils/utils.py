2e45303ceea518c20ca39bd8b8704dc9d63d8cda
Peiyu Liao <pyliao@stanford.edu>
2019-02-28 22:44:01 -0800

    Add reverse option to get date chunks

--- paperscraper/utils/utils.py
+++ paperscraper/utils/utils.py
@@ -1,13 +1,19 @@
 from datetime import datetime, timedelta
 
 
-def get_date_chunks(date_from, date_until, intv=30):
+def get_date_chunks(date_from, date_until, intv=30, reverse=True):
     start = datetime.strptime(date_from, '%Y-%m-%d')
     end = datetime.strptime(date_until, '%Y-%m-%d')
     intv = timedelta(intv)
 
-    s = start
-    while s + intv < end:
-        yield (s.strftime('%Y-%m-%d'), (s + intv).strftime('%Y-%m-%d'))
-        s += intv + timedelta(1)
-    yield (s.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'))
+    if reverse:
+        e = end
+        while e - intv > start:
+            yield ((e - intv).strftime('%Y-%m-%d'), e.strftime('%Y-%m-%d'))
+            e -= intv + timedelta(1)
+        yield (start.strftime('%Y-%m-%d'), e.strftime('%Y-%m-%d'))
+    else:
+        while s + intv < end:
+            yield (s.strftime('%Y-%m-%d'), (s + intv).strftime('%Y-%m-%d'))
+            s += intv + timedelta(1)
+        yield (s.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'))
